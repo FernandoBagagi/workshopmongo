619bf31c2bc29decd3de7a00ee13c3cde43f6045
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-12-28 10:10:31 -0300

    primeiro commit

---
+++ src/test/java/br/com/ferdbgg/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferdbgg.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
