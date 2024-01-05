7b3d4e38206044d6fb4b1c81aa44daf08447d313
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-05 10:05:38 -0300

    cria dados teste Instanciacao

--- src/main/java/br/com/ferdbgg/workshopmongo/configurations/Instanciacao.java
+++ src/main/java/br/com/ferdbgg/workshopmongo/configurations/Instanciacao.java
@@ -1,31 +1,54 @@
 package br.com.ferdbgg.workshopmongo.configurations;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.ferdbgg.workshopmongo.domain.Post;
 import br.com.ferdbgg.workshopmongo.domain.Usuario;
+import br.com.ferdbgg.workshopmongo.dtos.AutorDTO;
+import br.com.ferdbgg.workshopmongo.repositories.PostRepository;
 import br.com.ferdbgg.workshopmongo.repositories.UsuarioRepository;
 
 @Configuration
 public class Instanciacao implements CommandLineRunner {
 
     private final UsuarioRepository usuarioRepository;
+    private final PostRepository postRepository;
 
-    public Instanciacao(UsuarioRepository usuarioRepository) {
+    public Instanciacao(UsuarioRepository usuarioRepository, PostRepository postRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setTimeZone(TimeZone.getDefault());
+
         this.usuarioRepository.deleteAll();
+        this.postRepository.deleteAll();
+
         List<Usuario> usuariosDumb = new ArrayList<>();
         usuariosDumb.add(new Usuario(null, "Maria Brown", "maria@gmail.com"));
         usuariosDumb.add(new Usuario(null, "Alex Green", "alex@gmail.com"));
         usuariosDumb.add(new Usuario(null, "Carlos José", "carlos@gmail.com"));
         this.usuarioRepository.saveAll(usuariosDumb);
+
+        List<Post> postsDumb = new ArrayList<>();
+        postsDumb.add(new Post(null, simpleDateFormat.parse("2018-03-21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AutorDTO(usuariosDumb.get(0))));
+        postsDumb.add(new Post(null, simpleDateFormat.parse("2018-03-23"), "Bom dia", "Acordei feliz hoje!", new AutorDTO(usuariosDumb.get(0))));
+        this.postRepository.saveAll(postsDumb);
+
+        usuariosDumb.get(0).getPosts().add(postsDumb.get(0));
+        usuariosDumb.get(0).getPosts().add(postsDumb.get(1));
+        this.usuarioRepository.save(usuariosDumb.get(0));
+
     }
 
 }
