5ab0fe6acc8620006b1e79a1a29a564ae57e57fd
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-05 10:04:46 -0300

    cria atributo posts no Usuario

--- src/main/java/br/com/ferdbgg/workshopmongo/domain/Usuario.java
+++ src/main/java/br/com/ferdbgg/workshopmongo/domain/Usuario.java
@@ -1,8 +1,11 @@
 package br.com.ferdbgg.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.ferdbgg.workshopmongo.dtos.UsuarioDTO;
@@ -27,6 +30,15 @@ public class Usuario implements Serializable {
     private String id;
     private String nome;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts;
+
+    public Usuario(String id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.posts = new ArrayList<>();
+    }
 
     public Usuario(UsuarioDTO usuarioDTO) {
         this.id = usuarioDTO.getId();
