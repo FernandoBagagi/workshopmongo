354aa206f6c7d932bd9fad9ec2d3bed21db1feb1
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-08 09:04:04 -0300

    cria ComentarioDTO

---
+++ src/main/java/br/com/ferdbgg/workshopmongo/dtos/ComentarioDTO.java
@@ -0,0 +1,23 @@
+package br.com.ferdbgg.workshopmongo.dtos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ComentarioDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String texto;
+    private Date data;
+    private AutorDTO autor;
+
+}
