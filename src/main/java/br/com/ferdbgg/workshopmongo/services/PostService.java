5be47eedfd010e941e6b4f9934c59dad42a49f7c
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-11 09:53:20 -0300

    formata PostService

--- src/main/java/br/com/ferdbgg/workshopmongo/services/PostService.java
+++ src/main/java/br/com/ferdbgg/workshopmongo/services/PostService.java
@@ -12,9 +12,9 @@ import br.com.ferdbgg.workshopmongo.services.exceptions.ObjetoNaoEncontradoExcep
 
 @Service
 public class PostService {
-    
+
     private final PostRepository postRepository;
-    
+
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
@@ -29,13 +29,13 @@ public class PostService {
     }
 
     public List<Post> findByTituloContaining(String palavra) {
-        //return this.postRepository.findByTituloContainingIgnoreCase(palavra);
-        //return this.postRepository.findByTituloContainingIgnoreCase(palavra);
+        // return this.postRepository.findByTituloContainingIgnoreCase(palavra);
+        // return this.postRepository.findByTituloContainingIgnoreCase(palavra);
         return this.postRepository.encontrarPorTitulo(palavra);
     }
 
     public List<Post> findContaining(String palavra, Date dataMin, Date dataMax) {
-        dataMax = new Date(dataMax.getTime() + 24 * 60 * 60 * 1000); //Pra ir até 00:00 do outro dia
+        dataMax = new Date(dataMax.getTime() + 24 * 60 * 60 * 1000); // Pra ir até 00:00 do outro dia
         return this.postRepository.buscaCompleta(palavra, dataMin, dataMax);
     }
 
@@ -45,7 +45,7 @@ public class PostService {
 
     public void deleteById(String id) {
         Optional<Post> post = this.postRepository.findById(id);
-        if(post.isPresent()) {
+        if (post.isPresent()) {
             this.postRepository.deleteById(id);
         } else {
             throw new ObjetoNaoEncontradoException(id);
@@ -54,27 +54,27 @@ public class PostService {
 
     public Post update(String id, Post postNovosDados) {
         Optional<Post> post = this.postRepository.findById(id);
-        if(post.isPresent()) {
+        if (post.isPresent()) {
             final Post postBanco = post.get();
             this.atualizarDados(postBanco, postNovosDados);
-            return this.postRepository.save(postBanco);    
+            return this.postRepository.save(postBanco);
         } else {
             throw new ObjetoNaoEncontradoException(id);
         }
     }
 
     private void atualizarDados(Post postBanco, Post postNovosDados) {
-        //Verificar se a regra de negócio permite colocar null ou não
-        if(postNovosDados.getData() != null) {
+        // Verificar se a regra de negócio permite colocar null ou não
+        if (postNovosDados.getData() != null) {
             postBanco.setData(postNovosDados.getData());
         }
-        if(postNovosDados.getTitulo() != null) {
-            postBanco.setTitulo(postNovosDados.getTitulo()); 
+        if (postNovosDados.getTitulo() != null) {
+            postBanco.setTitulo(postNovosDados.getTitulo());
         }
-        if(postNovosDados.getCorpo() != null) {
+        if (postNovosDados.getCorpo() != null) {
             postBanco.setCorpo(postNovosDados.getCorpo());
         }
-        if(postNovosDados.getAutor() != null) {
+        if (postNovosDados.getAutor() != null) {
             postBanco.setAutor(postNovosDados.getAutor());
         }
     }
