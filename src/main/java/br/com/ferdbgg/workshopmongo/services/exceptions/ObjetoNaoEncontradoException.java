6a53691318009be4786abcd484b23a392f9908a0
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-01-11 09:52:56 -0300

    formata ObjetoNaoEncontradoException

--- src/main/java/br/com/ferdbgg/workshopmongo/services/exceptions/ObjetoNaoEncontradoException.java
+++ src/main/java/br/com/ferdbgg/workshopmongo/services/exceptions/ObjetoNaoEncontradoException.java
@@ -3,7 +3,7 @@ package br.com.ferdbgg.workshopmongo.services.exceptions;
 public class ObjetoNaoEncontradoException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
-    
+
     public ObjetoNaoEncontradoException(Object id) {
         super("Objeto de id " + id.toString() + " não foi encontrado");
     }
