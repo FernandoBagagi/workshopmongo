de32428cc5c025114bb9960aa862504db3dc8151
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-01-01 20:28:22 -0300

    adiciona proteção contra null no Instanciacao

--- src/main/java/br/com/ferdbgg/workshopmongo/configurations/Instanciacao.java
+++ src/main/java/br/com/ferdbgg/workshopmongo/configurations/Instanciacao.java
@@ -3,6 +3,7 @@ package br.com.ferdbgg.workshopmongo.configurations;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.TimeZone;
 
 import org.springframework.boot.CommandLineRunner;
@@ -39,24 +40,26 @@ public class Instanciacao implements CommandLineRunner {
         usuariosDumb.add(new Usuario(null, "Maria Brown", "maria@gmail.com"));
         usuariosDumb.add(new Usuario(null, "Alex Green", "alex@gmail.com"));
         usuariosDumb.add(new Usuario(null, "Carlos José", "carlos@gmail.com"));
-        this.usuarioRepository.saveAll(usuariosDumb);
+        usuariosDumb = this.usuarioRepository.saveAll(usuariosDumb);
 
         List<ComentarioDTO> comentariosDumb = new ArrayList<>();
         comentariosDumb.add(new ComentarioDTO("Boa viagem", simpleDateFormat.parse("2018-03-21"),
                 new AutorDTO(usuariosDumb.get(1))));
         comentariosDumb.add(new ComentarioDTO("Boa viagem!!", simpleDateFormat.parse("2018-03-22"),
                 new AutorDTO(usuariosDumb.get(2))));
+                
+        final var usuario1 = Objects.requireNonNull(usuariosDumb.get(0));
 
         List<Post> postsDumb = new ArrayList<>();
         postsDumb.add(new Post(null, simpleDateFormat.parse("2018-03-21"), "Partiu viagem",
-                "Vou viajar para São Paulo. Abraços!", new AutorDTO(usuariosDumb.get(0)), comentariosDumb));
+                "Vou viajar para São Paulo. Abraços!", new AutorDTO(usuario1), comentariosDumb));
         postsDumb.add(new Post(null, simpleDateFormat.parse("2018-03-23"), "Bom dia", "Acordei feliz hoje!",
-                new AutorDTO(usuariosDumb.get(0)), new ArrayList<>()));
-        this.postRepository.saveAll(postsDumb);
+                new AutorDTO(usuario1), new ArrayList<>()));
+        postsDumb = this.postRepository.saveAll(postsDumb);
 
-        usuariosDumb.get(0).getPosts().add(postsDumb.get(0));
-        usuariosDumb.get(0).getPosts().add(postsDumb.get(1));
-        this.usuarioRepository.save(usuariosDumb.get(0));
+        usuario1.getPosts().add(postsDumb.get(0));
+        usuario1.getPosts().add(postsDumb.get(1));
+        this.usuarioRepository.save(usuario1);
 
     }
 
